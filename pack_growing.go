c8776ad209c9fa58134b1a8d9bb788364ed5625e
Ryan Loader <ryan.loader@oneleggedcrab.com>
2015-10-07 09:56:41 +1300

    Refactor growing bin-packer

--- pack_growing.go
+++ pack_growing.go
@@ -2,146 +2,131 @@ package main
 
 import "fmt"
 
-type Node struct {
-	X     int
-	Y     int
-	W     int
-	H     int
-	Used  bool
-	Right *Node
-	Down  *Node
+type node struct {
+	x, y, w, h  int
+	right, down *node
+}
+
+func (n *node) clone() *node {
+	return &node{
+		x:     n.x,
+		y:     n.y,
+		w:     n.w,
+		h:     n.h,
+		right: n.right,
+		down:  n.down,
+	}
 }
 
 func PackGrowing(files []*File, maxWidth int, maxHeight int) {
 	w, h := 0, 0
-	if len(files) > 0 {
-		w = files[0].Width
-		h = files[0].Height
+	if len(files) == 0 {
+		return
 	}
-	root := &Node{
-		X: 0,
-		Y: 0,
-		W: w,
-		H: h,
+	w = files[0].Width
+	h = files[0].Height
+	root := &node{
+		x: 0,
+		y: 0,
+		w: w,
+		h: h,
 	}
 	for _, file := range files {
-		if node := findNode(root, file); node != nil {
-			_ = splitNode(node, file)
+		if node := root.find(file); node != nil {
+			_ = node.split(file)
 		} else {
-			_ = growNode(root, file, maxWidth, maxHeight)
+			_ = root.grow(file, maxWidth, maxHeight)
 		}
 	}
 }
 
-func findNode(root *Node, file *File) *Node {
-	fmt.Println("Find node", root, file)
-	if root.Used {
-		if res := findNode(root.Right, file); res != nil {
+func (n *node) find(file *File) *node {
+	fmt.Println("Find node", n, file)
+	if n.right != nil || n.down != nil {
+		if res := n.right.find(file); res != nil {
 			return res
 		} else {
-			return findNode(root.Down, file)
+			return n.down.find(file)
 		}
-	} else if file.Width <= root.W && file.Height <= root.H {
-		return root
+	} else if file.Width <= n.w && file.Height <= n.h {
+		return n
 	} else {
 		return nil
 	}
 }
 
-func splitNode(node *Node, file *File) *Node {
-	fmt.Println("Split node", node, file)
-	node.Used = true
-	file.X = node.X
-	file.Y = node.Y
-	fmt.Println("Changed the file", file)
-	node.Right = &Node{
-		X: node.X + file.Width,
-		Y: node.Y,
-		W: node.W - file.Width,
-		H: node.H + file.Height,
+func (n *node) split(file *File) *node {
+	fmt.Println("Split node", n, file)
+	file.X = n.x
+	file.Y = n.y
+	n.right = &node{
+		x: n.x + file.Width,
+		y: n.y,
+		w: n.w - file.Width,
+		h: n.h,
 	}
-	node.Down = &Node{
-		X: node.X,
-		Y: node.Y + file.Height,
-		W: node.W,
-		H: node.H - file.Height,
+	n.down = &node{
+		x: n.x,
+		y: n.y + file.Height,
+		w: n.w,
+		h: n.h - file.Height,
 	}
-	return node
+	return n
 }
 
-func growNode(root *Node, file *File, maxWidth int, maxHeight int) *Node {
-	fmt.Println("Grow node", root, file)
-	canGrowRight := file.Height <= root.H && root.W+file.Width < maxWidth
-	canGrowDown := file.Width <= root.W && root.H+file.Height < maxHeight
-	shouldGrowRight := canGrowRight && root.H >= root.W+file.Width
-	shouldGrowDown := canGrowDown && root.W >= root.H+file.Height
+func (n *node) grow(file *File, maxWidth int, maxHeight int) *node {
+	fmt.Println("Grow node", n, file)
+	canGrowRight := file.Height <= n.h && n.w+file.Width < maxWidth
+	canGrowDown := file.Width <= n.w && n.h+file.Height < maxHeight
+	shouldGrowRight := canGrowRight && n.h >= n.w+file.Width
+	shouldGrowDown := canGrowDown && n.w >= n.h+file.Height
 
 	if shouldGrowRight {
-		return growRight(root, file)
+		return n.growRight(file)
 	} else if shouldGrowDown {
-		return growDown(root, file)
+		return n.growDown(file)
 	} else if canGrowRight {
-		return growRight(root, file)
+		return n.growRight(file)
 	} else if canGrowDown {
-		return growDown(root, file)
+		return n.growDown(file)
 	} else {
 		return nil
 	}
 }
 
-func growRight(root *Node, file *File) *Node {
-	fmt.Println("Grow right", root, file)
-	oldRoot := &Node{
-		X:     root.X,
-		Y:     root.Y,
-		W:     root.W,
-		H:     root.H,
-		Used:  root.Used,
-		Right: root.Right,
-		Down:  root.Down,
+func (n *node) growRight(file *File) *node {
+	fmt.Println("Grow right", n, file)
+	prev := n.clone()
+	n.x, n.y = 0, 0
+	n.w = prev.w + file.Width
+	n.right = &node{
+		x: prev.w,
+		y: 0,
+		w: file.Width,
+		h: prev.h,
 	}
-	root.X = 0
-	root.Y = 0
-	root.W = oldRoot.W + file.Width
-	root.H = oldRoot.H
-	root.Right = &Node{
-		X: oldRoot.W,
-		Y: 0,
-		W: file.Width,
-		H: oldRoot.H,
-	}
-	root.Down = oldRoot
-	if node := findNode(root, file); root != nil {
-		return splitNode(node, file)
+	n.down = prev
+	if next := n.find(file); next != nil {
+		return next.split(file)
 	} else {
 		return nil
 	}
 }
 
-func growDown(root *Node, file *File) *Node {
-	fmt.Println("Grow down", root, file)
-	oldRoot := &Node{
-		X:     root.X,
-		Y:     root.Y,
-		W:     root.W,
-		H:     root.H,
-		Used:  root.Used,
-		Right: root.Right,
-		Down:  root.Down,
+func (n *node) growDown(file *File) *node {
+	fmt.Println("Grow down", n, file)
+	prev := n.clone()
+	n.x, n.y = 0, 0
+	n.h = prev.h + file.Height
+	n.right = prev
+	n.down = &node{
+		x: 0,
+		y: prev.h,
+		w: prev.w,
+		h: file.Height,
 	}
-	root.X = 0
-	root.Y = 0
-	root.W = oldRoot.W
-	root.H = oldRoot.H + file.Height
-	root.Right = oldRoot
-	root.Down = &Node{
-		X: 0,
-		Y: oldRoot.H,
-		W: oldRoot.W,
-		H: file.Height,
-	}
-	if node := findNode(root, file); root != nil {
-		return splitNode(node, file)
+	if next := n.find(file); next != nil {
+		return next.split(file)
 	} else {
 		return nil
 	}
